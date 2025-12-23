7b3ba9f22bbf6c6e093744feb920c1058aa7da7e
nkouflie <98182534+nkouflie@users.noreply.github.com>
2025-12-22 19:10:48 -0500

    Initial Commit

---
+++ AlarmV2/AlarmV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmV2App.swift
+//  AlarmV2
+//
+//  Created by Nicholas Kouflie on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AlarmV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
